5700f445a835fa346f2c9188f9d6db8be42555cb
vistritpandey <vistritpandey@gmail.com>
2020-11-30 12:09:28 +0530

    k

--- social/src/screens/SignUpScreen.js
+++ social/src/screens/SignUpScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export default function SignInScreen() {
+export default function SignUpScreen() {
   return (
     <View>
       <Text></Text>
